853d333620fbb299834a4d65040a29fc62dff7f9
Alan Storm <astorm@alanstorm.com>
2014-07-14 16:31:31 -0700

    removing old hard coded key

--- app/code/community/Segment/Analytics/Helper/Data.php
+++ app/code/community/Segment/Analytics/Helper/Data.php
@@ -4,7 +4,6 @@ class Segment_Analytics_Helper_Data extends Mage_Core_Helper_Abstract
     public function getWriteKey()
     {        
         return Mage::getStoreConfig('general/segment_analytics/write_key');
-        // return '0n1mip8hyd';
     }
     
     public function isAdmin()
