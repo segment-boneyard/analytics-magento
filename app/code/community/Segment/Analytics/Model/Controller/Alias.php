ce2a92aa700820df362a7f65e46f573eb4e58dfd
Alan Storm <astorm@alanstorm.com>
2014-07-15 19:58:24 -0700

    Issue #26 -- one alias per customer

--- app/code/community/Segment/Analytics/Model/Controller/Alias.php
+++ app/code/community/Segment/Analytics/Model/Controller/Alias.php
@@ -4,6 +4,11 @@ class Segment_Analytics_Model_Controller_Alias extends Segment_Analytics_Model_C
     public function getBlock($block)
     {
         $customer = $this->_getCustomerSession();
+        if($customer->getCustomer()->getData('is_segment_aliased'))
+        {
+            return false;
+        }
+        $customer->getCustomer()->setData('is_segment_aliased','1')->save();
         $block->setUserId($customer->getId());                        
         return $block;       
     }
