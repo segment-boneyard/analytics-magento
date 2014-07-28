1c2a04aca9510168d4dd1a20c6d24fd0f85d052d
Alan Storm <astorm@alanstorm.com>
2014-07-28 15:22:34 -0700

    issue #45

--- app/code/community/Segment/Analytics/Model/Controller/Orderplaced.php
+++ app/code/community/Segment/Analytics/Model/Controller/Orderplaced.php
@@ -8,19 +8,19 @@ class Segment_Analytics_Model_Controller_Orderplaced extends Segment_Analytics_M
         $info    = Mage::getModel('sales/order_api')
         ->info($params['increment_id']);
         
-        $params['total']            = $info['grand_total'];
+        $params['total']            = (float) $info['grand_total'];
         $params['status']           = $info['status'];        
-        $params['shipping']         = $info['shipping_amount'];
-        $params['tax']              = $info['tax_amount'];
+        $params['shipping']         = (float) $info['shipping_amount'];
+        $params['tax']              = (float) $info['tax_amount'];
         $params['products']         = array();
         foreach($info['items'] as $item)
         {
             $tmp = array();      
             $tmp['sku']           = $item['sku'];
             $tmp['name']          = $item['name'];;
-            $tmp['price']         = $item['price'];
-            $tmp['quantity']      = $item['qty_ordered'];
-            $tmp['product_id']    = $item['product_id'];            
+            $tmp['price']         = (float) $item['price'];
+            $tmp['quantity']      = (float) $item['qty_ordered'];
+            $tmp['product_id']    = (int) $item['product_id'];            
 
             $params['products'][] = $tmp;
         }
