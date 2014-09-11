de4d1e7d3de9d8f179756c6bda8a4728d3a4f71c
Alan Storm <astorm@alanstorm.com>
2014-09-10 21:25:02 -0700

    Issue #62

--- app/code/community/Segment/Analytics/Model/Controller/Identity.php
+++ app/code/community/Segment/Analytics/Model/Controller/Identity.php
@@ -15,6 +15,10 @@ class Segment_Analytics_Model_Controller_Identity extends Segment_Analytics_Mode
         ->getSource()
         ->getOptionText($customer->getData('gender'));
         
+        $address    = Mage::getModel('customer/address')->load(
+            $customer->getDefaultBilling()
+        );
+
         $block->setUserId($customer->getId())
         ->addData($customer->getData())
         ->setFullName($customer->getName())
@@ -41,6 +45,18 @@ class Segment_Analytics_Model_Controller_Identity extends Segment_Analytics_Mode
         ->unsetData('lastname')
         ->unsetData('middlename');
         
+        if($address)
+        {
+            $region = Mage::getModel('directory/region')->load($address->getRegionId());
+            $street = $address->getStreet();
+            $street = implode("\n", $street);
+            $block
+            ->setData('city',       $address->getCity())
+            ->setData('country',    $address->getCountryId())
+            ->setData('postalCode', $address->getPostcode())
+            ->setData('state',      $region->getCode())
+            ->setData('street',     $street);
+        }
         $data = $block->getData();        
         $data = Mage::helper('segment_analytics')->getNormalizedCustomerInformation($data);
         $block->setData($data);
