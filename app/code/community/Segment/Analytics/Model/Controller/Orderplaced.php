a501ab2cc75fedff52ba7da7f4a1b9aab3d22874
Josh Klina <joshua.klina@gmail.com>
2015-03-24 17:32:58 -0700

    adding functionality to record discounts and revenue

--- app/code/community/Segment/Analytics/Model/Controller/Orderplaced.php
+++ app/code/community/Segment/Analytics/Model/Controller/Orderplaced.php
@@ -9,9 +9,11 @@ class Segment_Analytics_Model_Controller_Orderplaced extends Segment_Analytics_M
         ->info($params['increment_id']);
 
         $params['total']            = (float) $info['grand_total'];
+        $params['revenue']          = (float) $info['subtotal'];
         $params['status']           = $info['status'];
         $params['shipping']         = (float) $info['shipping_amount'];
         $params['tax']              = (float) $info['tax_amount'];
+        $params['discount']         = (-1 * (float) $info['discount_amount']);
         $params['products']         = array();
         foreach($info['items'] as $item)
         {
